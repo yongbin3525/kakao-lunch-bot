6f2405653a0b7d3515b0525c1961a567b864bdb4
yongbin3525 <57318732+yongbin3525@users.noreply.github.com>
2020-11-25 14:04:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 let year;
 let month;
 let day;
@@ -35,7 +49,7 @@ example();
 
 
 const apiRouter = express.Router();
-
+app.use(allowCrossDomain);
 app.use(logger('dev', {}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
