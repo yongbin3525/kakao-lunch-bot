1b8095ba557f45997a48e2e84376554d86afd3ff
yongbin3525 <57318732+yongbin3525@users.noreply.github.com>
2020-11-25 14:27:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 
-var allowCrossDomain = function(req, res, next) {
+const allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
