44d14253e25b673d56d3e05167870103841a54d5
yongbin3525 <57318732+yongbin3525@users.noreply.github.com>
2020-11-25 14:12:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var allowCrossDomain = function(req, res, next) {
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
-      res.send(200);
+      res.send('Hello');
     }
     else {
       next();
